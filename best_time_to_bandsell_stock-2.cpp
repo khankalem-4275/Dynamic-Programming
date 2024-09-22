22ac0528c59aff54a74a2db9c2cc22b0238d8d07
khankalem-4275 <khankalem.4275@gmail.com>
2024-09-22 12:18:26 +0530

    memoization approach also

--- best_time_to_bandsell_stock-2.cpp
+++ best_time_to_bandsell_stock-2.cpp
@@ -29,3 +29,37 @@ public:
         return dp[0][1];
     }
 };
+
+
+// using memoization
+
+class Solution {
+public:
+    
+    int solve(int index , int buy , vector<int> &prices , int n,
+             vector<vector<int>> &dp){
+        if(index == n) return 0;
+        
+        int profit = 0;
+        
+        if(dp[index][buy] != -1) return dp[index][buy];
+        
+        if(buy){
+            profit = max(-prices[index] + solve(index+1,0,prices,n,dp) ,
+                         solve(index+1,1,prices,n,dp));
+        }
+        else{
+            profit = max(prices[index] + solve(index+1,1,prices,n,dp) ,
+                         solve(index+1,0,prices,n,dp));
+        }
+        
+        return dp[index][buy] = profit;
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        // changing parameters => index , buy
+        int n = prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return solve(0,1,prices,n,dp);
+    }
+};
